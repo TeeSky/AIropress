b6b7cc6c72866968ff3bd2068c864ffffd6e3bd3
Tom Skypala <xskypala@gmail.com>
2019-08-16 23:16:54 +0200

    refactor(flow): create folder structure and move tdd code to project

---
+++ AIropress/AIropress/Model/BrewParameters.swift
@@ -0,0 +1,14 @@
+//
+//  BrewParameters.swift
+//  AIropress
+//
+//  Created by Skypy on 16/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
+
+
+struct BrewParameters: Equatable {
+    
+}
