ee8af10ceccaef0e69c23521201b4a7d1c14b015
Tom Skypala <xskypala@gmail.com>
2019-08-16 14:15:35 +0200

    build(project): init xcode project

---
+++ AIropress/AIropress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIropress
+//
+//  Created by Skypy on 14/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
