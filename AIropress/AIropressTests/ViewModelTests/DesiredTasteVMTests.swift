d63325b47cb96652bc2aeb098c2f6ba929b800e9
Tom Skypala <xskypala@gmail.com>
2019-08-18 00:59:56 +0200

    test(vm): finish desiredtastevm test implementation

--- AIropress/AIropressTests/ViewModelTests/DesiredTasteVMTests.swift
+++ AIropress/AIropressTests/ViewModelTests/DesiredTasteVMTests.swift
@@ -106,4 +106,44 @@ class DesiredTasteVMTests: XCTestCase {
         XCTAssertNotEqual(unexpectedBrewParameters1, desiredTasteVM.brewParameters)
         XCTAssertNotEqual(unexpectedBrewParameters2, desiredTasteVM.brewParameters)
     }
+    
+    func testNumberOfSections() {
+        XCTAssertEqual(1, desiredTasteVM.numberOfSections())
+    }
+    
+    func testNumberOfRows() {
+        let expectedNumberOfRows = brewVariableBundles.count
+        
+        XCTAssertEqual(expectedNumberOfRows, desiredTasteVM.numberOfRows(section: 0))
+    }
+    
+    func testCellViewModelForPath(){
+        let expectedCell1VMBrewVariableBundle = brewVariableBundles[0]
+        let expectedCell2VMBrewVariableBundle = brewVariableBundles[1]
+        
+        let cell1VM = desiredTasteVM.cellViewModel(for: IndexPath(row: 0, section: 0))
+        XCTAssertEqual(expectedCell1VMBrewVariableBundle,
+                       (cell1VM as? BrewVariableBundleCellVM)?.variableBundle)
+        
+        let cell2VM = desiredTasteVM.cellViewModel(for: IndexPath(row: 1, section: 0))
+        XCTAssertEqual(expectedCell2VMBrewVariableBundle,
+                       (cell2VM as? BrewVariableBundleCellVM)?.variableBundle)
+    }
+    
+    func testOnCalculateClicked() {
+        let flowController = MockDesiredTasteFlowController()
+        let expectedParameters = BrewParameters(brewVariableBundles: brewVariableBundles, values: [:])
+        desiredTasteVM.flowController = flowController
+        
+        let brewVariableChange = (brewVariableBundles[0].variables[0], 0.1)
+        // Change assurance
+        XCTAssertNotEqual(expectedParameters.valueMap[brewVariableChange.0.id], brewVariableChange.1)
+        expectedParameters.valueMap[brewVariableChange.0.id] = brewVariableChange.1
+        desiredTasteVM.cellVMs[0].valueDelegate?.onValueChanged(brewVariable: brewVariableChange.0, value: brewVariableChange.1)
+        
+        desiredTasteVM.onCalculateClicked()
+        
+        XCTAssertNotNil(flowController.setParameters)
+        XCTAssertEqual(expectedParameters, flowController.setParameters)
+    }
 }
