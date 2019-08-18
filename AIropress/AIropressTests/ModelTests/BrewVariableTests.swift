1d25e8036626e7d1bd3410e8b161c4f31aec1eb6
Tom Skypala <xskypala@gmail.com>
2019-08-18 02:26:19 +0200

    style: unify created by in file headers

--- AIropress/AIropressTests/ModelTests/BrewVariableTests.swift
+++ AIropress/AIropressTests/ModelTests/BrewVariableTests.swift
@@ -2,7 +2,7 @@
 //  BrewVariableTests.swift
 //  AIropressTests
 //
-//  Created by Skypy on 17/08/2019.
+//  Created by Tomas Skypala on 17/08/2019.
 //  Copyright © 2019 Tomas Skypala. All rights reserved.
 //
 
