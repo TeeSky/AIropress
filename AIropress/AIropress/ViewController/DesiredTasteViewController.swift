1fb955eab792a5ac3b7b675d27e1aa7a1c0e1319
Tom Skypala <xskypala@gmail.com>
2019-08-18 17:58:32 +0200

    feat(vc): implement desiredtasteviewcontroller

---
+++ AIropress/AIropress/ViewController/DesiredTasteViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DesiredTasteViewController.swift
+//  AIropress
+//
+//  Created by Tomas Skypala on 18/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
