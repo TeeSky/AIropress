1941ad1eca978679c6afd7e2dbe75bc688e52a40
Tom Skypala <xskypala@gmail.com>
2019-08-19 02:09:32 +0200

    feat(vc): init desiredtastesceneview

--- AIropress/AIropress/SceneView/DesiredTasteSceneView.swift
+++ AIropress/AIropress/SceneView/DesiredTasteSceneView.swift
@@ -8,11 +8,55 @@
 
 import Foundation
 import UIKit
+import TinyConstraints
 
 class DesiredTasteSceneView: BaseSceneView {
     
-    var tableView: UITableView?
+    var sceneLabel: UILabel!
+    var calculateButton: UIButton!
+    var tableView: UITableView!
     
     override func render() {
+        self.backgroundColor = .white
+        
+        setupLabel()
+        setupCalculateButton()
+//        setupTableView(under: sceneLabel, above: calculateButton)
+    }
+    
+    private func setupLabel() {
+        sceneLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        sceneLabel.text = "Desired"
+        sceneLabel.textColor = .black
+        sceneLabel.textAlignment = .left
+        sceneLabel.font = UIFont.systemFont(ofSize: 35, weight: .heavy)
+        
+        self.addSubview(sceneLabel)
+        
+        sceneLabel.topToSuperview(offset: 75)
+        sceneLabel.leftToSuperview(offset: 15)
+        sceneLabel.rightToSuperview()
+    }
+    
+    private func setupCalculateButton() {
+        calculateButton = UIButton(type: .roundedRect)
+        calculateButton.frame = CGRect(x: 0, y: 0, width: 250, height: 150)
+        calculateButton.backgroundColor = .green
+        calculateButton.setTitle("Calculate", for: .normal)
+        
+        self.addSubview(calculateButton)
+        
+        calculateButton.centerInSuperview()
+    }
+    
+    private func setupTableView(under label: UIView, above: UIView) {
+        tableView = UITableView()
+
+        self.addSubview(tableView)
+        
+        tableView.topToBottom(of: label)
+        tableView.leftToSuperview()
+        tableView.rightToSuperview()
+        tableView.bottomToTop(of: above)
     }
 }
