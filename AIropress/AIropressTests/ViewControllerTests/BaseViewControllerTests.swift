61a13bd80c1fc82652e592846581ff4ce1ec5cd0
Tom Skypala <xskypala@gmail.com>
2019-08-19 23:24:31 +0200

    test(vc): implement baseviewcontrollertests

---
+++ AIropress/AIropressTests/ViewControllerTests/BaseViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewControllerTests.swift
+//  AIropressTests
+//
+//  Created by Tomas Skypala on 19/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
