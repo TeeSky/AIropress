9e0c6dab026bcc78a4069b87f60b311a850e1523
Tom Skypala <xskypala@gmail.com>
2019-08-20 18:24:08 +0200

    test(vc): implement baseviewcontrollertests

--- AIropress/AIropressTests/ViewControllerTests/BaseViewControllerTests.swift
+++ AIropress/AIropressTests/ViewControllerTests/BaseViewControllerTests.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2019 Tomas Skypala. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class MockSceneView: BaseSceneView {
+
+    var didCallRender: Bool = false
+    
+    override func render() {
+        didCallRender = true
+    }
+}
+
+class BaseViewControllerTests: XCTestCase {
+    
+    var viewController: BaseViewController<MockSceneView>!
+    
+    override func setUp() {
+        super.setUp()
+        
+        viewController = BaseViewController()
+    }
+    
+    func testInit() {
+        XCTAssertNotNil(viewController.sceneView)
+    }
+    
+    func testRenderOnViewDidLoad() {
+        viewController.viewDidLoad()
+        
+        XCTAssertTrue(viewController.sceneView.didCallRender)
+    }
+}
