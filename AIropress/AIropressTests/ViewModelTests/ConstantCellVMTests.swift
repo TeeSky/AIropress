389034790a7402ee1e24987a7924acfcc29b98a8
Tom Skypala <xskypala@gmail.com>
2019-08-22 18:27:40 +0200

    refactor(vm): move constantcellvm and semiconstantcellvm tdd code to project

--- AIropress/AIropressTests/ViewModelTests/ConstantCellVMTests.swift
+++ AIropress/AIropressTests/ViewModelTests/ConstantCellVMTests.swift
@@ -8,39 +8,6 @@
 
 import XCTest
 
-class ConstantCellVM {
-    
-    static let cellIdentifier: String = {
-        return "ConstantCellVM"
-    }()
-    
-    var cellLabel: String {
-        return constant.label
-    }
-    
-    var cellValueText: String {
-        return constant.valueText
-    }
-    
-    private let constant: RecipeConstant
-    
-    init(constant: RecipeConstant) {
-        self.constant = constant
-    }
-}
-
-extension ConstantCellVM: BaseTableCellVM {
-    
-    var identifier: String {
-        return ConstantCellVM.cellIdentifier
-    }
-    
-    var cellHeight: CGFloat {
-        return 35
-    }
-    
-}
-
 class ConstantCellVMTests: XCTestCase {
     
     var recipeConstant: RecipeConstant!
--- AIropress/AIropressTests/ViewModelTests/SemiConstantCellVMTests.swift
+++ AIropress/AIropressTests/ViewModelTests/SemiConstantCellVMTests.swift
@@ -8,62 +8,6 @@
 
 import XCTest
 
-class SemiConstantCellVM {
-    
-    static let cellIdentifier: String = {
-        return "SemiConstantCellVM"
-    }()
-    
-    static let cellHeight: CGFloat = {
-        let labelHeight = 35
-        let sliderHeight = BrewVariableSlider.height
-        return CGFloat(labelHeight + sliderHeight)
-    }()
-    
-    var cellLabel: String {
-        return recipeConstant.label
-    }
-    
-    var cellValueText: String {
-        return recipeConstant.valueText
-    }
-    
-    var recipeConstant: RecipeConstant {
-        return semiConstant.constant
-    }
-    
-    var confidenceVariable: BrewVariable {
-        return semiConstant.confidenceVariable
-    }
-    
-    var confidenceValue: Double {
-        return changedConfidenceValue ?? semiConstant.confidenceValue
-    }
-    
-    private let semiConstant: RecipeSemiConstant
-    private var changedConfidenceValue: Double?
-    
-    init(semiConstant: RecipeSemiConstant) {
-        self.semiConstant = semiConstant
-    }
-    
-    func onSliderValueChanged(to value: Float) {
-        changedConfidenceValue = Double(value)
-    }
-}
-
-extension SemiConstantCellVM: BaseTableCellVM {
-    
-    var identifier: String {
-        return SemiConstantCellVM.cellIdentifier
-    }
-    
-    var cellHeight: CGFloat {
-        return SemiConstantCellVM.cellHeight
-    }
-    
-}
-
 class SemiConstantCellVMTests: XCTestCase {
 
     var recipeSemiConstant: RecipeSemiConstant!
