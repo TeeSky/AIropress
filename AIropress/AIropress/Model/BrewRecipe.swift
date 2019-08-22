23fae537b88994c189a6abdb757fb4862b45a9e2
Tom Skypala <xskypala@gmail.com>
2019-08-22 02:20:20 +0200

    feat(model): implement brewrecipe and associated classes

--- AIropress/AIropress/Model/BrewRecipe.swift
+++ AIropress/AIropress/Model/BrewRecipe.swift
@@ -9,5 +9,39 @@
 import Foundation
 
 struct BrewRecipe: Equatable {
+    let constants: [RecipeConstant]
+    let semiConstants: [RecipeSemiConstant]
+}
+
+class RecipeConstant {
+    let id: Double
+    let label: String
     
+    let value: Double
+    let valueText: String
+    
+    init(id: Double, label: String, value: Double, valueText: String) {
+        self.id = id
+        self.label = label
+        self.value = value
+        self.valueText = valueText
+    }
+}
+
+extension RecipeConstant: Equatable {
+    
+    static func == (lhs: RecipeConstant, rhs: RecipeConstant) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+}
+
+class RecipeSemiConstant: RecipeConstant {
+    var confidence: Double
+    
+    init(id: Double, label: String, value: Double, valueText: String, confidence: Double) {
+        self.confidence = confidence
+        
+        super.init(id: id, label: label, value: value, valueText: valueText)
+    }
 }
