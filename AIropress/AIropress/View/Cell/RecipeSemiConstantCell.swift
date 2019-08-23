970f4628fe8cf9e5044392d4ccbe866e47963e54
Tom Skypala <xskypala@gmail.com>
2019-08-23 21:42:47 +0200

    feat(view): implement recipesemiconstantcell

--- AIropress/AIropress/View/Cell/RecipeSemiConstantCell.swift
+++ AIropress/AIropress/View/Cell/RecipeSemiConstantCell.swift
@@ -9,6 +9,31 @@
 import Foundation
 import UIKit
 
-class RecipeSemiConstantCell: UITableViewCell {
+class RecipeSemiConstantCell: BaseTableViewCell<RecipeSemiConstantCellView> {
     
+}
+
+extension RecipeSemiConstantCell: ConfigurableTableCell {
+    
+    func configure(viewModel: BaseTableCellVM) {
+        guard let viewModel = viewModel as? SemiConstantCellVM else { fatalError("Unexpected view model type.") }
+        
+        setupConstantView(viewModel: viewModel)
+        setupConfidenceSlider(viewModel: viewModel)
+        
+        didSetConstraints = false
+        self.updateConstraints()
+    }
+    
+    private func setupConstantView(viewModel: SemiConstantCellVM) {
+        cellView.constantView.constantLabel.text = viewModel.cellLabel
+        cellView.constantView.constantValue.text = viewModel.cellValueText
+    }
+    
+    private func setupConfidenceSlider(viewModel: SemiConstantCellVM) {
+        cellView.confidenceSlider.configure(brewVariable: viewModel.confidenceVariable, initialValue: Float(viewModel.confidenceValue))
+        cellView.confidenceSlider.delegate = { sliderValue in
+            viewModel.onSliderValueChanged(to: sliderValue.raw)
+        }
+    }
 }
