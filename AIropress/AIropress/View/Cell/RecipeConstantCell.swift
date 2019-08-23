89db46e22b0c66c57ea59dc9e02f9af88f8486bc
Tom Skypala <xskypala@gmail.com>
2019-08-23 17:34:13 +0200

    feat(view): add recipeconstantcell and recipesemiconstantcell

---
+++ AIropress/AIropress/View/Cell/RecipeConstantCell.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeConstantCell.swift
+//  AIropress
+//
+//  Created by Tomas Skypala on 23/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeConstantCell: UITableViewCell {
+    // TODO implement
+}
---
+++ AIropress/AIropress/View/Cell/RecipeSemiConstantCell.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeSemiConstantCell.swift
+//  AIropress
+//
+//  Created by Tomas Skypala on 23/08/2019.
+//  Copyright © 2019 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeSemiConstantCell: UITableViewCell {
+    
+}
