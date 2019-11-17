b01f31eec03b0fc92aae709d290dce91f0920ebb
Tom Skypala <xskypala@gmail.com>
2019-11-18 00:22:23 +0100

    rf(vm): rework desiredtastevm as variablebundlevm

--- AIropress/AIropress/ViewModel/DesiredTasteVM.swift
+++ AIropress/AIropress/ViewModel/DesiredTasteVM.swift
@@ -9,56 +9,12 @@
 import Foundation
 import UIKit
 
-class DesiredTasteVM {
-
-    private(set) var cellVMs: [BrewVariableBundleCellVM]
-    let brewParameters: BrewParameters
+class DesiredTasteVM: VariableBundleVM {
 
     weak var flowController: DesiredTasteSceneFC?
-
-    init(brewVariableBundles: [BrewVariableBundle], values: [BrewVariable.Id: Double?] = [:]) {
-        brewParameters = BrewParameters(brewVariableBundles: brewVariableBundles, values: values)
-        cellVMs = []
-
-        setupCellVMs(brewVariableBundles: brewVariableBundles)
-    }
-
-    func configure(tableView: UITableView) {
-        tableView.register(BrewVariableBundleCell.self, forCellReuseIdentifier: BrewVariableBundleCellVM.cellIdentifier)
-    }
 
     @objc
     func onCalculateClicked() {
         flowController?.onParametersSet(brewParameters: brewParameters)
-    }
-
-    private func setupCellVMs(brewVariableBundles: [BrewVariableBundle]) {
-        for variableBundle in brewVariableBundles {
-            //            var initialValues: [BrewVariable: Double] = variableBundle.variables.reduce([:]) { $0[$1] = brewParameters.valueMap[$1.id] }
-            let initialValues = variableBundle.variables
-                .reduce([BrewVariable: Double]()) { (dict, variable) -> [BrewVariable: Double] in
-                    var dict = dict
-                    dict[variable] = brewParameters.valueMap[variable.id]
-                    return dict
-                }
-
-            let vm = BrewVariableBundleCellVM(variableBundle: variableBundle, initialValues: initialValues)
-            vm.valueDelegate = self
-            cellVMs.append(vm)
-        }
-    }
-}
-
-extension DesiredTasteVM: VariableBundleCellValueDelegate {
-
-    func onValueChanged(brewVariable: BrewVariable, value: Double) {
-        brewParameters.valueMap[brewVariable.id] = value
-    }
-}
-
-extension DesiredTasteVM: BaseTableVM {
-
-    var cellViewModels: [BaseTableCellVM] {
-        return cellVMs
     }
 }
